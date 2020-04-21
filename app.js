8c67ce7deeb34bb0eef3f6d4aafee3efce0b5d70
unknown <prabhat.avb@gmail.com>
2020-04-22 03:11:26 +0530

    version3

--- app.js
+++ app.js
@@ -1,53 +1,29 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
+var seedDB = require("./seeds.js");
 
 mongoose.connect("mongodb://localhost/Yelp_camp", {useNewUrlParser: true,  useUnifiedTopology: true});
 //bodyparser helps in retrieving the data from the form(builds the req.body object, later it can be used to pick the form data)
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: false}));
 
-//SCHEMA SETUP 
-var campgroundSchema = new mongoose.Schema({
-    title: String,
-    img: String,
-    description: String
-});
+//This is used to add some starter data directly to server. Thus the name Seed.
+seedDB();
 
-var Campground = mongoose.model("Campground",campgroundSchema);
-
-/* Campground.create(
-    {title: "Salmon creek", 
-    img: "https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    description: "Beautiful Salmon ground, has everything that you need for a perfect camping. Visit this camping site for a thrilling experience."
-},
-    function(err,campground){
-    if(err){
-        console.log("ERROR OCCURED!" + err);
-    }
-    else{
-        console.log("VALUE INSERTED");
-    }
-}); */
-
-//array of objects to hold the data of the campgrounds.
-/* var campgrounds = [
-    {title: "Salmon creek", img: "https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-    {title: "Mallon", img: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-    {title: "Ranted", img: "https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-    {title: "Locali", img: "https://images.unsplash.com/photo-1520095972714-909e91b038e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"},
-    {title: "All nighter", img: "https://images.unsplash.com/photo-1490452322586-70484206da38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"},
-    {title: "Calen clean", img: "https://images.unsplash.com/photo-1494112142672-801c71472ba5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"},
-    {title: "Mount Goat", img: "https://images.unsplash.com/photo-1564577160324-112d603f750f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"}
-]; */
+//GETTING THE MODEL FROM OTHER FILES INSIDE MODELS FOLDER.(THIS INCREASES REUSABILITY AND MINIMIZES THE APP.JS FILE) 
+var Campground = require("./models/campgroundSchema.js");
 
 //routes
+
+//ROOT ROUTE
 app.get("/",function(req,res){
 
     res.render("home.ejs");
 
 });
 
+//INDEX ROUTE
 app.get("/campgrounds",function(req,res){
     Campground.find({},function(err, allCampgrounds){
         if(err){
@@ -59,6 +35,13 @@ app.get("/campgrounds",function(req,res){
     });
 });
 
+
+//NEW ROUTE
+app.get("/campgrounds/new", function(req,res){
+    res.render("new.ejs");
+});
+
+//CREATE ROUTE
 app.post("/campgrounds", function(req,res){
 
     //get data from the form
@@ -83,18 +66,16 @@ app.post("/campgrounds", function(req,res){
     res.redirect("/campgrounds");
 });
 
-app.get("/campgrounds/new", function(req,res){
-    res.render("new.ejs");
-});
-
+//SHOW ROUTE
 app.get("/campgrounds/:id",function(req,res){
     var id = req.params.id;
     //find the campground with this ID
-    Campground.findById(id, function(err,foundCampground){
+    Campground.findById(id).populate("comments").exec(function(err,foundCampground){
         if(err){
             console.log("Error"+ err);
         }
         else{
+            
             res.render("show.ejs",{campground: foundCampground});
         }
     });
